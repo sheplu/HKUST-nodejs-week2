10a16e9c43bce70773df5956b5ea7ece04542765
Sheplu <jean.burellier@gmail.com>
2016-03-27 20:36:43 +0200

    add new models

--- models/promotions.js
+++ models/promotions.js
@@ -4,27 +4,8 @@ var Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 var Currency = mongoose.Types.Currency;
 
-var commentSchema = new Schema({
-    rating:  {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: true
-    },
-    comment:  {
-        type: String,
-        required: true
-    },
-    author:  {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
-});
-
 // create a schema
-var dishSchema = new Schema({
+var promoSchema = new Schema({
     name: {
         type: String,
         required: true,
@@ -35,30 +16,25 @@ var dishSchema = new Schema({
         required: true,
         unique: true
     },
-    category: {
-        type: String,
-        required: true
-    },
     label: {
-      type: String,
-      default:""
+        type: String,
+        default:""
     },
     price: {
-      type: Currency,
-      required: true
+        type: Currency,
+        required: true
     },
     description: {
         type: String,
         required: true
-    },
-    comments:[commentSchema]
+    }
 }, {
     timestamps: true
 });
 
 // the schema is useless so far
 // we need to create a model using it
-var Dishes = mongoose.model('Dish', dishSchema);
+var Promotions = mongoose.model('Promotion', promoSchema);
 
 // make this available to our Node applications
-module.exports = Dishes;
+module.exports = Promotions;
