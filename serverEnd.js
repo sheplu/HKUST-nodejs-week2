20d2e87040911ce3d5aceb27fdbd1d94908e872b
Sheplu <jean.burellier@gmail.com>
2016-03-27 20:36:13 +0200

    update server

--- serverEnd.js
+++ serverEnd.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'),
     assert = require('assert');
 
-var Dishes = require('./models/dishes');
+var Dishes = require('./models/promotions');
 
 // Connection URL
 var url = 'mongodb://localhost:27017/conFusion';mongoose.connect(url);
@@ -13,60 +13,18 @@ db.once('open', function () {
 
     // create a new dish
     Dishes.create({
-        name: 'Uthapizza',
-        image: "images/uthapizza.png",
-        category: "mains",
-        label: "Hot",
-        price: "4.99",
-        description: 'Test',
-        comments: [
-            {
-                rating: 3,
-                comment: 'This is insane',
-                author: 'Matt Daemon'
-            },
-            {
-                rating: 4,
-                comment: 'sdfsdf',
-                author: 'xvxcvxcvxcvn'
-            }
-        ]
-    }, function (err, dish) {
+      "name": "Weekend Grand Buffet",
+      "image": "images/buffet.png",
+      "label": "New",
+      "price": "19.99",
+      "description": "Featuring . . ."
+}, function (err, dish) {
         if (err) throw err;
         console.log('Dish created!');
         console.log(dish);
 
         var id = dish._id;
 
-        // get all the dishes
-        setTimeout(function () {
-            Dishes.findByIdAndUpdate(id, {
-                    $set: {
-                        description: 'Updated Test'
-                    }
-                }, {
-                    new: true
-                })
-                .exec(function (err, dish) {
-                    if (err) throw err;
-                    console.log('Updated Dish!');
-                    console.log(dish);
 
-                    dish.comments.push({
-                        rating: 5,
-                        comment: 'I\'m getting a sinking feeling!',
-                        author: 'Leonardo di Carpaccio'
-                    });
-
-                    dish.save(function (err, dish) {
-                        console.log('Updated Comments!');
-                        console.log(dish);
-
-                        db.collection('dishes').drop(function () {
-                            db.close();
-                        });
-                    });
-                });
-        }, 3000);
     });
 });
